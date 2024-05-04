06e9faaa3ea94dab1a88e82bcc257935b3e4b6c2
mozza-p <pramozza@gmail.com>
2024-05-04 11:36:11 +0700

    tambahan intent implicit dial phone

--- app/src/main/java/com/example/intent/HomeActivity.kt
+++ app/src/main/java/com/example/intent/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.example.intent
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -32,14 +34,23 @@ class HomeActivity : AppCompatActivity(), View. OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btnExplicit -> {
-                //fungsi inntent dijalankan di dalam aksi button atau onclick
+                //fungsi intent dijalankan di dalam aksi button atau onclick
                 //deklarasi intent
                 val intentExplicit = Intent(this@HomeActivity, PageActivity2::class.java)
                 // run intent
                 startActivity(intentExplicit)
             }
             R.id.btnImplicit -> {
-
+                // Intent implicit untuk melakukan panggilan telepon
+                val noTelp = "08111236789" // Nomor telepon yang akan dihubungi
+                val dialIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel: $noTelp"))
+                // Pemeriksaan apakah ada aplikasi yang dapat menangani panggilan telepon
+                if (dialIntent.resolveActivity(packageManager) != null) {
+                    startActivity(dialIntent)
+                } else {
+                    // Notifikasi jika tidak ditemukan aplikasi tersebut
+                    Toast.makeText(this, "Tidak ada aplikasi untuk melakukan panggilan telepon.", Toast.LENGTH_LONG).show()
+                }
             }
         }
     }
